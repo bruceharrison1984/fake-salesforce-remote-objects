39552a24fc06d2ebc76867aae4a7c55c73077633
Bruce Harrison <leeharrison1984@gmail.com>
2018-02-26 22:40:08 -0600

    Add additional logger color

--- logger.js
+++ logger.js
@@ -4,10 +4,13 @@ class logger{
     console.debug(`%c ${message}`, 'color: green; font-weight:bold;');
   }
 
+  static logDebug(message) {
+    console.debug(`%c ${message}`, 'color: darkblue; font-weight:bold;');
+  }
+
   static logError(message) {
     console.error(message);
   }
-
 }
 
 export default logger;
