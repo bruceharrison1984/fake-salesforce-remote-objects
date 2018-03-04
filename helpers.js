59a2e90b912aad469bcc7f4b91493f56e8eec0f0
Bruce Harrison <leeharrison1984@gmail.com>
2018-03-03 22:14:34 -0600

    Add some additional helper functions

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+import uuid from 'uuid/v4';
+
 export function getJsNamespace() {
   const remoteObjectNodes = document.getElementsByTagName('apex:remoteObjects');
   if (!remoteObjectNodes.length) {
@@ -10,18 +12,34 @@ export function getJsNamespace() {
   return jsNamespaceArray[0];
 }
 
+export function createObjectId() {
+  return uuid()
+    .split('-')
+    .slice(0, 3)
+    .join('');
+}
+
+export function removeCustomFields(obj) {
+  let newObject = {};
+  Object.keys(obj).filter(field => field.indexOf('_') !== 0).map(field => newObject[field] = obj[field]);
+  return newObject;
+}
+
 export function getRemoteObjectModel() {
   const remoteObjectModelNodes = document.getElementsByTagName('apex:remoteobjectmodel');
   const jsObjectModelArray = Array.from(remoteObjectModelNodes, element => {
     return {
       name: element.getAttribute('name'),
       jsshorthand: element.getAttribute('jsshorthand'),
-      fields: element.getAttribute('fields').split(',').filter(x => x !== "")
+      fields: element
+        .getAttribute('fields')
+        .split(',')
+        .filter(x => x !== '')
     };
   });
   const objectsWithNoFields = jsObjectModelArray.filter(x => x.fields.length === 0);
   if (objectsWithNoFields.length) {
-    throw new Error(`apex:remoteobjectmodel doesn't have any fields defined: ${objectsWithNoFields.map(x => x.name).join(';')}`)
+    throw new Error(`apex:remoteobjectmodel doesn't have any fields defined: ${objectsWithNoFields.map(x => x.name).join(';')}`);
   }
   const jsShorthandNames = jsObjectModelArray.map(x => x.jsshorthand.trim());
   if (jsShorthandNames.filter(getUniqueValues).length < jsObjectModelArray.length) {
