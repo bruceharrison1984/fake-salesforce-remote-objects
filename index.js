d5d7c6554d53cb197cbfb5a5eed29d3dfd5b3d63
Bruce Harrison <leeharrison1984@gmail.com>
2018-03-04 00:18:34 -0600

    Cleanup startup

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ import logger from './src/logger';
 import { getJsNamespace, getRemoteObjectModel } from './src/helpers';
 
 class sfRemoteObject {
-  constructor(jsNamespace, remoteObjectModels) {
+  constructor() {
+    this.jsNamespace = getJsNamespace();
+    if (this.jsNamespace === undefined) {
+      logger.logDebug('No apex:remoteObjects could be found.\nThis typically means you have deployed fake-salesforce-remote-objects in to Salesforce.\nYour page will work normally, but you don\'t need to include this library when deploying.');
+      return;
+    }
+    this.remoteObjectModels = getRemoteObjectModel();
+
     logger.logDebug('Fake SF Remote Objects Active');
-    for (let i = 0; i < remoteObjectModels.length; i++) {
-      const remoteObjectModel = remoteObjectModels[i];
-      logger.logDebug(`Setting up fake remote object - Namespace: ${jsNamespace} | SFType: '${remoteObjectModel.name}' | JsShorthand: '${remoteObjectModel.jsshorthand}' \n Fields: ${JSON.stringify(remoteObjectModel.fields, null, 2)}`);
+    for (let i = 0; i < this.remoteObjectModels.length; i++) {
+      const remoteObjectModel = this.remoteObjectModels[i];
+      logger.logDebug(`Setting up fake remote object - Namespace: ${this.jsNamespace} | SFType: '${remoteObjectModel.name}' | JsShorthand: '${remoteObjectModel.jsshorthand}' \n Fields: ${JSON.stringify(remoteObjectModel.fields, null, 2)}`);
       this[remoteObjectModel.name] = function (obj) {
         return new remoteObject(obj, remoteObjectModel.name, remoteObjectModel.jsshorthand, remoteObjectModel.fields);
       };
@@ -17,19 +24,11 @@ class sfRemoteObject {
         };
       }
     }
+
+    window[this.jsNamespace] = this;
   }
 }
 
-function buildRemoteObjectController() {
-  let jsNamespace = getJsNamespace();
-  if (jsNamespace === undefined) {
-    logger.logDebug('No apex:remoteObjects could be found.\nThis typically means you have deployed fake-salesforce-remote-objects in to Salesforce.\nYour page will work normally, but you don\'t need to include this library when deploying.');
-    return;
-  }
-  let remoteObjectModels = getRemoteObjectModel();
-  window[jsNamespace] = new sfRemoteObject(jsNamespace, remoteObjectModels);
-}
-
-buildRemoteObjectController();
+new sfRemoteObject();
 
 export default sfRemoteObject;
