7d95443d3c69cae434522502b18ec2e8a19c999f
Bruce Harrison <leeharrison1984@gmail.com>
2018-03-04 19:17:57 -0600

    Rename fields to use private naming

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ import { getJsNamespace, getRemoteObjectModel } from './src/helpers';
 
 class sfRemoteObject {
   constructor() {
-    this.jsNamespace = getJsNamespace();
-    if (this.jsNamespace === undefined) {
+    this._jsNamespace = getJsNamespace();
+    if (this._jsNamespace === undefined) {
       logger.logDebug('No apex:remoteObjects could be found.\nThis typically means you have deployed fake-salesforce-remote-objects in to Salesforce.\nYour page will work normally, but you don\'t need to include this library when deploying.');
       return;
     }
-    this.remoteObjectModels = getRemoteObjectModel();
+    this._remoteObjectModels = getRemoteObjectModel();
 
     logger.logDebug('Fake SF Remote Objects Active');
-    for (let i = 0; i < this.remoteObjectModels.length; i++) {
-      const remoteObjectModel = this.remoteObjectModels[i];
+    for (let i = 0; i < this._remoteObjectModels.length; i++) {
+      const remoteObjectModel = this._remoteObjectModels[i];
       logger.logDebug(`Setting up fake remote object - Namespace: ${this.jsNamespace} | SFType: '${remoteObjectModel.sfObjectType}' | JsShorthand: '${remoteObjectModel.shorthandName}' \n Fields: ${JSON.stringify(remoteObjectModel.definedFields, null, 2)}`);
       this[remoteObjectModel.sfObjectType] = function (obj) {
         return new remoteObject(obj, remoteObjectModel);
@@ -25,7 +25,7 @@ class sfRemoteObject {
       }
     }
 
-    window[this.jsNamespace] = this;
+    window[this._jsNamespace] = this;
   }
 }
 
