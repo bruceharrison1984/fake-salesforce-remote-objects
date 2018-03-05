b2f160fd1b4bd0b517396ac2986f77b7dd7d5bc2
Bruce Harrison <leeharrison1984@gmail.com>
2018-03-04 20:16:35 -0600

    Refactor create further

--- src/remoteObjectMethods/create.js
+++ src/remoteObjectMethods/create.js
@@ -4,43 +4,35 @@ import { hasCallback, createObjectId, removeCustomFields } from '../helpers';
 export default function create(argOne, argTwo, remoteObject) {
   let _remoteObject = remoteObject;
   switch (hasCallback(argOne, argTwo)) {
-    case 'NoCallback': {
-      return noCallback(_remoteObject);
-    }
-    case 'CallbackWithoutValues': {
-      return callbackWithoutValues(argOne, _remoteObject);
-    }
-    case 'CallbackWithValues': {
-      return callbackWithValues(argOne, argTwo, _remoteObject);
-    }
-    case 'Error': {
-      return error(_remoteObject);
-    }
+    case 'NoCallback': return noCallback(_remoteObject);
+    case 'CallbackWithoutValues': return callbackWithoutValues(argOne, _remoteObject);
+    case 'CallbackWithValues': return callbackWithValues(argOne, argTwo, _remoteObject);
+    case 'Error': return error(_remoteObject);
   }
+}
 
-  function noCallback(_remoteObject) {
-    logger.logInfo(`Inserting ${_remoteObject._sfObjectType} record in to Salesforce without callback:\n${JSON.stringify(removeCustomFields(_remoteObject), null, 2)}`);
-    _remoteObject.Id = createObjectId();
-    return _remoteObject;
-  }
+function noCallback(_remoteObject) {
+  logger.logInfo(`Inserting ${_remoteObject._sfObjectType} record in to Salesforce without callback:\n${JSON.stringify(removeCustomFields(_remoteObject), null, 2)}`);
+  _remoteObject.Id = createObjectId();
+  return _remoteObject;
+}
 
-  function callbackWithoutValues(callback, _remoteObject) {
-    logger.logInfo(`Inserting ${_remoteObject._sfObjectType} record in to Salesforce with callback:\n${JSON.stringify(removeCustomFields(_remoteObject), null, 2)}`);
-    _remoteObject.Id = createObjectId();
-    return callback();
-  }
+function callbackWithoutValues(callback, _remoteObject) {
+  logger.logInfo(`Inserting ${_remoteObject._sfObjectType} record in to Salesforce with callback:\n${JSON.stringify(removeCustomFields(_remoteObject), null, 2)}`);
+  _remoteObject.Id = createObjectId();
+  return callback();
+}
 
-  function callbackWithValues(values, callback, _remoteObject) {
-    if (values.Id) {
-      logger.logError(`Inserting ${_remoteObject._sfObjectType} record failed. Cannot have Id field when creating record`);
-    }
-    logger.logInfo(`Inserting ${_remoteObject._sfObjectType} record in to Salesforce with callback: ${JSON.stringify(values, null, 2)}`);
-    _remoteObject.Id = createObjectId();
-    return callback();
+function callbackWithValues(values, callback, _remoteObject) {
+  if (values.Id) {
+    logger.logError(`Inserting ${_remoteObject._sfObjectType} record failed. Cannot have Id field when creating record`);
   }
+  logger.logInfo(`Inserting ${_remoteObject._sfObjectType} record in to Salesforce with callback: ${JSON.stringify(values, null, 2)}`);
+  _remoteObject.Id = createObjectId();
+  return callback();
+}
 
-  function error(_remoteObject) {
-    logger.logError(`Inserting ${_remoteObject._sfObjectType} record failed. First argument must either be a function or an object, second argument can only be a function`);
-    return _remoteObject;
-  }
+function error(_remoteObject) {
+  logger.logError(`Inserting ${_remoteObject._sfObjectType} record failed. First argument must either be a function or an object, second argument can only be a function`);
+  return _remoteObject;
 }
