5a90c431606ac148854aa7176dd8f96e39160f22
Bruce Harrison <leeharrison1984@gmail.com>
2018-03-04 20:17:11 -0600

    Refactor update to make it easier to read

--- src/remoteObjectMethods/update.js
+++ src/remoteObjectMethods/update.js
@@ -4,30 +4,38 @@ import { hasCallback, removeCustomFields } from '../helpers';
 export default function updateRemoteObject(argOne, argTwo, remoteObject) {
   let _remoteObject = remoteObject;
   switch (hasCallback(argOne, argTwo)) {
-    case ('NoCallback'): {
-      if (!_remoteObject.Id || _remoteObject.Id === '') {
-        logger.logError(`Updating ${_remoteObject._sfObjectType} record failed. Id must be specified.`);
-      }
-      logger.logInfo(`Updating ${_remoteObject._sfObjectType} record in to Salesforce without callback:\n${JSON.stringify(removeCustomFields(_remoteObject), null, 2)}`);
-      return _remoteObject;
-    }
-    case ('CallbackWithoutValues'):{
-      if (!_remoteObject.Id || _remoteObject.Id === '') {
-        logger.logError(`Updating ${_remoteObject._sfObjectType} record failed. Id must be specified.`);
-      }
-      logger.logInfo(`Updating ${_remoteObject._sfObjectType} record in to Salesforce with callback:\n${JSON.stringify(removeCustomFields(_remoteObject), null, 2)}`);
-      return argOne();
-    }
-    case ('CallbackWithValues'):{
-      if (!argOne.Id || argOne.Id === '') {
-        logger.logError(`Updating ${_remoteObject._sfObjectType} record failed. Id must be specified.`);
-      }
-      logger.logInfo(`Updating ${_remoteObject._sfObjectType} record in to Salesforce with callback: ${JSON.stringify(argOne, null, 2)}`);
-      return argTwo();
-    }
-    case('Error'): {
-      logger.logError(`Updating ${_remoteObject._sfObjectType} record failed. First argument must either be a function or an object, second argument can only be a function`);
-      return _remoteObject;
-    }
+    case 'NoCallback': return noCallback(_remoteObject);
+    case 'CallbackWithoutValues': return callbackWithoutValues(argOne, _remoteObject);
+    case 'CallbackWithValues': return callbackWithValues(argOne, argTwo, _remoteObject);
+    case 'Error': return error(_remoteObject);
   }
+}
+
+function noCallback(_remoteObject) {
+  if (!_remoteObject.Id || _remoteObject.Id === '') {
+    logger.logError(`Updating ${_remoteObject._sfObjectType} record failed. Id must be specified.`);
+  }
+  logger.logInfo(`Updating ${_remoteObject._sfObjectType} record in to Salesforce without callback:\n${JSON.stringify(removeCustomFields(_remoteObject), null, 2)}`);
+  return _remoteObject;
+}
+
+function callbackWithoutValues(callback, _remoteObject) {
+  if (!_remoteObject.Id || _remoteObject.Id === '') {
+    logger.logError(`Updating ${_remoteObject._sfObjectType} record failed. Id must be specified.`);
+  }
+  logger.logInfo(`Updating ${_remoteObject._sfObjectType} record in to Salesforce with callback:\n${JSON.stringify(removeCustomFields(_remoteObject), null, 2)}`);
+  return callback();
+}
+
+function callbackWithValues(values, callback, _remoteObject) {
+  if (!values.Id || values.Id === '') {
+    logger.logError(`Updating ${_remoteObject._sfObjectType} record failed. Id must be specified.`);
+  }
+  logger.logInfo(`Updating ${_remoteObject._sfObjectType} record in to Salesforce with callback: ${JSON.stringify(values, null, 2)}`);
+  return callback();
+}
+
+function error(_remoteObject) {
+  logger.logError(`Updating ${_remoteObject._sfObjectType} record failed. First argument must either be a function or an object, second argument can only be a function`);
+  return _remoteObject;
 }
