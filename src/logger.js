7c440209a11652aac72bdbcbbea8ef2867610ec8
Bruce Harrison <leeharrison1984@gmail.com>
2018-03-08 00:09:37 -0600

    Add log levels to logger

--- src/logger.js
+++ src/logger.js
@@ -1,15 +1,15 @@
 class logger{
 
   static logInfo(message) {
-    console.debug(`%c ${message}`, 'color: green; font-weight:bold;');
+    console.debug(`%c [INFO] ${message}`, 'color: green; font-weight:bold;');
   }
 
   static logDebug(message) {
-    console.debug(`%c ${message}`, 'color: darkblue; font-weight:bold;');
+    console.debug(`%c [DEBUG] ${message}`, 'color: darkblue; font-weight:bold;');
   }
 
   static logError(message) {
-    console.error(message);
+    console.error(`[ERROR] ${message}`);
   }
 }
 
